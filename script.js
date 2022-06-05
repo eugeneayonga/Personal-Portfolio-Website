7cb51a5d12f8dbef70acc105d14b39f9df9364c3
Eugene <eugeneayonga@gmail.com>
2022-06-05 20:03:16 +0300

    Updated homepage slide, Responsive Media Query

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-const h2 = document.createElement("h2");
+/*const h2 = document.createElement("h2");
 h2.textContent = "This content added by Javascript";
 
-document.querySelector("body").appendChild(h2);
+document.querySelector("body").appendChild(h2);*/
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollX > 18){
+            $(".navBar").addClass(".zeller");
+        }else{
+            $(".navBar").removeClass(".zeller");
+        }
+    });
+
+    // menu-navBar toggle script
+    $(".menu-btn").click(function(){
+        $(".navBar .menu").toggleClass("active");
+        $(".menu-btn i").toggleClass("active");
+    });
+});
